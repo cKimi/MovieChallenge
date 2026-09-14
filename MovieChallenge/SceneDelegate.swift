e58b0353b29172cb24e8b27989071fc3ffa26efc
Carlos Kimura <ckimura@gmail.com>
2026-09-14 15:44:14 -0300

    chore: configure UIKit app without storyboard

---
+++ MovieChallenge/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  MovieChallenge
+//
+//  Created by Carlos Kimura on 14/09/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        let window = UIWindow(windowScene: windowScene)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
---
+++ MovieChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieChallenge
+//
+//  Created by Carlos Kimura on 14/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Movies"
+    }
+}
+
